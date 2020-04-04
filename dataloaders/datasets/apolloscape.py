cb283892c8ed94d11236caa8e96abd3c835c4b49
Fikrat.Gasimov <qasimov.fikrat@gmail.com>
2020-04-04 10:51:55 +0200

    Updated

--- dataloaders/datasets/apolloscape.py
+++ dataloaders/datasets/apolloscape.py
@@ -154,7 +154,7 @@ class ApolloScapeSegmentation(Dataset):
     def transform_tr(self, sample):
         composed_transforms = transforms.Compose([
             tr.RandomHorizontalFlip(),
-            tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
+            tr.RandomCrop(crop_size=self.args.crop_size),
             tr.RandomGaussianBlur(),
             tr.Normalize(mean=(0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)),
             tr.ToTensor()])
